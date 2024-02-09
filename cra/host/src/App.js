931cfe940c7c1c5184c3e2da17d32eae3ba4035b
AdrienBot <adrien.biguet.75@gmail.com>
2024-02-09 16:38:06 +0100

    Add Pokemon Fake API Call

--- cra/host/src/App.js
+++ cra/host/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../sharedcomponent/shared/Header';
 import Footer from '../../sharedcomponent/shared/Footer';
 import UniqueHostComponent from './UniqueHostComponent';
 
-const RemoteButton = React.lazy(() => import('remote/Button'));
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
 
-const App = () => (
-  <div>
-    <Header />
-    <UniqueHostComponent title="Host App Unique Title" />
-    <h1>Basic Host-Remote</h1>
-    <h2>Host</h2>
-    <React.Suspense fallback="Loading Button">
-      <RemoteButton />
-    </React.Suspense>
-    <Footer />
-  </div>
-);
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+      .then(response => response.json())
+      .then(data => setPokemons(data.results));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <UniqueHostComponent title="Host App Unique Title" />
+      <h1>Pokémon List</h1>
+      <ul>
+        {pokemons.map(pokemon => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
