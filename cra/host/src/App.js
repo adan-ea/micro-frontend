1258b7a225be0b542807aca3ebb08a728b5f65e6
AdrienBot <adrien.biguet.75@gmail.com>
2024-02-09 15:41:46 +0100

    Added unique component

--- cra/host/src/App.js
+++ cra/host/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../sharedcomponent/shared/Header';
 import Footer from '../../sharedcomponent/shared/Footer';
+import UniqueHostComponent from './UniqueHostComponent';
 
 const RemoteButton = React.lazy(() => import('remote/Button'));
 
 const App = () => (
   <div>
     <Header />
+    <UniqueHostComponent title="Host App Unique Title" />
     <h1>Basic Host-Remote</h1>
     <h2>Host</h2>
     <React.Suspense fallback="Loading Button">
---
+++ cra/host/src/UniqueHostComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UniqueHostComponent = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>This is a unique component for the Host application.</p>
+    </div>
+  );
+};
+
+export default UniqueHostComponent;
